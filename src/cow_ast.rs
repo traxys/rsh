190fcb83c7c137bc2edb35522b9f09974f3758cd
Quentin Boyer <qbsecond@gmail.com>
2021-09-28 20:13:11 +0200

    enable to own the ast

--- src/cow_ast.rs
+++ src/cow_ast.rs
@@ -24,6 +24,30 @@ impl<'a> From<ast::StringPart<'a>> for StringPart<'a> {
     }
 }
 
+fn cow_to_owned(cow: CowStr<'_>) -> CowStr<'static> {
+    match cow {
+        Cow::Borrowed(b) => Cow::Owned(b.to_owned()),
+        Cow::Owned(o) => Cow::Owned(o),
+    }
+}
+
+fn convert_vec<I, O, F>(i: Vec<I>, f: F) -> Vec<O>
+where
+    F: Fn(I) -> O,
+{
+    i.into_iter().map(f).collect()
+}
+
+impl<'a> StringPart<'a> {
+    pub fn owned<'b>(self) -> StringPart<'b> {
+        match self {
+            StringPart::Text(v) => StringPart::Text(cow_to_owned(v)),
+            StringPart::Variable(v) => StringPart::Variable(v),
+            StringPart::Expression(e) => StringPart::Expression(e.owned()),
+        }
+    }
+}
+
 pub type RedirectionType = ast::RedirectionType;
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -34,6 +58,16 @@ pub struct Command<'input> {
     pub args: Vec<Expression<'input>>,
     #[serde(borrow)]
     pub redirections: Vec<(RedirectionType, Expression<'input>)>,
+}
+
+impl<'a> Command<'a> {
+    pub fn owned<'b>(self) -> Command<'b> {
+        Command {
+            name: self.name.owned(),
+            args: convert_vec(self.args, |e| e.owned()),
+            redirections: convert_vec(self.redirections, |(v, e)| (v, e.owned())),
+        }
+    }
 }
 
 impl<'a> From<ast::Command<'a>> for Command<'a> {
@@ -56,6 +90,14 @@ pub struct Pipeline<'input> {
     pub commands: Vec<Command<'input>>,
 }
 
+impl<'a> Pipeline<'a> {
+    pub fn owned<'b>(self) -> Pipeline<'b> {
+        Pipeline {
+            commands: convert_vec(self.commands, |e| e.owned()),
+        }
+    }
+}
+
 impl<'a> From<ast::Pipeline<'a>> for Pipeline<'a> {
     fn from(v: ast::Pipeline<'a>) -> Self {
         Self {
@@ -70,6 +112,15 @@ pub enum ChainPart<'input> {
     Pipeline(Pipeline<'input>),
     #[serde(borrow)]
     Chain(Box<CommandChain<'input>>),
+}
+
+impl<'a> ChainPart<'a> {
+    pub fn owned<'b>(self) -> ChainPart<'b> {
+        match self {
+            ChainPart::Pipeline(p) => ChainPart::Pipeline(p.owned()),
+            ChainPart::Chain(c) => ChainPart::Chain(Box::new(c.owned())),
+        }
+    }
 }
 
 impl<'a> From<ast::ChainPart<'a>> for ChainPart<'a> {
@@ -87,6 +138,16 @@ pub enum CommandChain<'input> {
     And(ChainPart<'input>, Box<CommandChain<'input>>),
     #[serde(borrow)]
     Pipeline(Pipeline<'input>),
+}
+
+impl<'a> CommandChain<'a> {
+    pub fn owned<'b>(self) -> CommandChain<'b> {
+        match self {
+            CommandChain::Or(p, r) => CommandChain::Or(p.owned(), Box::new(r.owned())),
+            CommandChain::And(p, r) => CommandChain::And(p.owned(), Box::new(r.owned())),
+            CommandChain::Pipeline(p) => CommandChain::Pipeline(p.owned()),
+        }
+    }
 }
 
 impl<'a> From<ast::CommandChain<'a>> for CommandChain<'a> {
@@ -111,6 +172,16 @@ pub struct VariableDefinition<'input> {
     pub ty: Type,
 }
 
+impl<'a> VariableDefinition<'a> {
+    pub fn owned<'b>(self) -> VariableDefinition<'b> {
+        VariableDefinition {
+            name: self.name,
+            ty: self.ty,
+            expr: self.expr.owned(),
+        }
+    }
+}
+
 impl<'a> From<ast::VariableDefinition<'a>> for VariableDefinition<'a> {
     fn from(v: ast::VariableDefinition<'a>) -> Self {
         Self {
@@ -129,6 +200,15 @@ pub struct CommandContext<'input> {
     pub variables: Vec<VariableDefinition<'input>>,
 }
 
+impl<'a> CommandContext<'a> {
+    pub fn owned<'b>(self) -> CommandContext<'b> {
+        CommandContext {
+            commands: convert_vec(self.commands, |c| c.owned()),
+            variables: convert_vec(self.variables, |v| v.owned()),
+        }
+    }
+}
+
 impl<'a> From<ast::CommandContext<'a>> for CommandContext<'a> {
     fn from(v: ast::CommandContext<'a>) -> Self {
         Self {
@@ -144,6 +224,17 @@ pub enum CommandStatement<'input> {
     Definitions(Vec<VariableDefinition<'input>>),
     #[serde(borrow)]
     Commands(CommandContext<'input>),
+}
+
+impl<'a> CommandStatement<'a> {
+    pub fn owned<'b>(self) -> CommandStatement<'b> {
+        match self {
+            CommandStatement::Definitions(d) => {
+                CommandStatement::Definitions(convert_vec(d, |v| v.owned()))
+            }
+            CommandStatement::Commands(c) => CommandStatement::Commands(c.owned()),
+        }
+    }
 }
 
 impl<'a> From<ast::CommandStatement<'a>> for CommandStatement<'a> {
@@ -166,6 +257,18 @@ pub enum Statement<'input> {
         blk: Vec<CommandStatement<'input>>,
         capture: bool,
     },
+}
+
+impl<'a> Statement<'a> {
+    pub fn owned<'b>(self) -> Statement<'b> {
+        match self {
+            Statement::VarDef(v) => Statement::VarDef(v.owned()),
+            Statement::Cmd { blk, capture } => Statement::Cmd {
+                capture,
+                blk: convert_vec(blk, |c| c.owned()),
+            },
+        }
+    }
 }
 
 impl<'a> From<ast::Statement<'a>> for Statement<'a> {
@@ -206,6 +309,14 @@ impl<'input> Value<'input> {
             Value::List(_) => Type::List(Box::new(Type::Dynamic)),
         }
     }
+
+    pub fn owned<'b>(self) -> Value<'b> {
+        match self {
+            Value::String(s) => Value::String(cow_to_owned(s)),
+            Value::Int(i) => Value::Int(i),
+            Value::List(l) => Value::List(convert_vec(l, |e| e.owned())),
+        }
+    }
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -224,6 +335,26 @@ pub enum Expression<'input> {
     Interpolated(Vec<StringPart<'input>>),
     SubShell(Box<CommandContext<'input>>),
     Variable(Spur),
+}
+
+impl<'a> Expression<'a> {
+    pub fn owned<'b>(self) -> Expression<'b> {
+        match self {
+            Expression::Value(v) => Expression::Value(v.owned()),
+            Expression::Call { function, args } => Expression::Call {
+                function: Box::new(function.owned()),
+                args: convert_vec(args, |a| a.owned()),
+            },
+            Expression::Method { value, name, args } => Expression::Method {
+                value: Box::new(value.owned()),
+                args: convert_vec(args, |arg| arg.owned()),
+                name,
+            },
+            Expression::Interpolated(i) => Expression::Interpolated(convert_vec(i, |p| p.owned())),
+            Expression::SubShell(ctx) => Expression::SubShell(Box::new(ctx.owned())),
+            Expression::Variable(v) => Expression::Variable(v),
+        }
+    }
 }
 
 impl<'a> From<ast::Expression<'a>> for Expression<'a> {
